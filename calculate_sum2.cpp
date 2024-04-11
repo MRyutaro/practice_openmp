b46f23e8d7ef74e6beed9ebcf18ababedb6a6123
MRyutaro <m2002r1028@icloud.com>
2024-04-11 11:14:17 +0900

    add: 並列化したときとしてないときで、処理時間がどれくらい変わるかを計測してみた

--- calculate_sum2.cpp
+++ calculate_sum2.cpp
@@ -12,16 +12,17 @@ int main()
     double start, end;
     start = omp_get_wtime();
 
+    cout << "Start: " << start << endl;
     // 総和を求める
-    #pragma omp parallel for reduction(+:val)
     for (long int i = 0; i < 1000000000; i++)
     {
         val += i;
     }
 
     end = omp_get_wtime();
+    cout << "End: " << end << endl;
     cout << "Sum: " << val << endl;
-    cout << "Time: " << end - start << "s" << endl;  // 並列化すると、だいたい0.3sくらい
+    cout << "Time: " << end - start << "s" << endl;  // 並列化しないと、だいたい2sくらい
 
     return 0;
 }
